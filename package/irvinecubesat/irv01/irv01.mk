f98d6b0584d75a0227dcd2a62de54116ca51337f
Maurice Ling <maurice@linglink.com>
2016-09-13 22:12:01 -0700

    add irvinecubesat/irv01 packages

---
+++ package/irvinecubesat/irv01/irv01.mk
@@ -0,0 +1,31 @@
+####
+#
+# Irvine 01 cubesat software package.  
+#
+####
+
+IRV01_VERSION = 0.0
+IRV01_SOURCE=irvine-01-sw-$(IRV01_VERSION).tar.gz
+IRV01_SITE=https://github.com/irvinecubesat/irvine-01-sw
+IRV01_SITE_METHOD=git
+#
+# For libraries this would be YES
+#
+IRV01_INSTALL_STAGING = NO
+
+#
+# Install to target
+#
+IRV01_INSTALL_TARGET = YES
+
+IRV01_DEPENDENCIES=libproc libsatpkt libgpio
+
+ifeq ($(BR2_PACKAGE_IRV01_version_custom),y)
+	IRV01_VERSION=$(subst ",,$(BR2_PACKAGE_IRV01_CONFIG_CUSTOM_VERSION_STR))
+endif
+
+ifeq ($(BR2_PACKAGE_IRV01_version_head),y)
+   IRV01_VERSION:=$(shell git ls-remote $(IRV01_SITE) | grep HEAD | head -1 | sed -e 's/[ \t]*HEAD//')
+endif
+
+$(eval $(call CMAKETARGETS,package,irv01))
