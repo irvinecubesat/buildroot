04f955b1e85699ab9a9e7c6f3c523a95874cbc40
Maurice Ling <maurice@linglink.com>
2016-09-23 06:59:02 -0700

    Fix irvinecubesat project inclusion and add WITH_BUILDROOT flag for ir01.mk

--- package/irvinecubesat/irv01/irv01.mk
+++ package/irvinecubesat/irv01/irv01.mk
@@ -8,6 +8,8 @@ IRV01_VERSION = 0.0
 IRV01_SOURCE=irvine-01-sw-$(IRV01_VERSION).tar.gz
 IRV01_SITE=https://github.com/irvinecubesat/irvine-01-sw
 IRV01_SITE_METHOD=git
+IRV01_SUPPORTS_IN_SOURCE_BUILD = NO
+IRV01_CONF_OPT=-DWITH_BUILDROOT=ON
 #
 # For libraries this would be YES
 #
@@ -18,7 +20,7 @@ IRV01_INSTALL_STAGING = NO
 #
 IRV01_INSTALL_TARGET = YES
 
-IRV01_DEPENDENCIES=libproc libsatpkt libgpio
+IRV01_DEPENDENCIES=polysat_fsw
 
 ifeq ($(BR2_PACKAGE_IRV01_version_custom),y)
 	IRV01_VERSION=$(subst ",,$(BR2_PACKAGE_IRV01_CONFIG_CUSTOM_VERSION_STR))
@@ -27,5 +29,7 @@ endif
 ifeq ($(BR2_PACKAGE_IRV01_version_head),y)
    IRV01_VERSION:=$(shell git ls-remote $(IRV01_SITE) | grep HEAD | head -1 | sed -e 's/[ \t]*HEAD//')
 endif
+
+$(eval $(cmake-package))
 
 $(eval $(call CMAKETARGETS,package,irv01))
---
+++ package/irvinecubesat/irvinecubesat.mk
@@ -0,0 +1,1 @@
+include package/irvinecubesat/*/*.mk
