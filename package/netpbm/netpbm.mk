b9161e01c3b4b87e6619364c6af0d950e73cbccd
Maurice Ling <maurice@linglink.com>
2016-12-21 07:11:17 -0800

    netpbm mods

--- package/netpbm/netpbm.mk
+++ package/netpbm/netpbm.mk
@@ -4,27 +4,32 @@
 #
 #############################################################
 
-NETPBM_VERSION = 10.35.88
+NETPBM_VERSION = 10.47.63
 NETPBM_SOURCE:=netpbm-$(NETPBM_VERSION).tgz
+
 NETPBM_SITE=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/netpbm/super_stable/$(NETPBM_VERSION)
 NETPBM_CONFIGURE_PREFIX=/usr/local
 NETPBM_CONFIGURE_EXEC_PREFIX=/usr/local
 #NETPBM_INSTALL_STAGING=YES
 NETPBM_INSTALL_TARGET=YES
 
-#   make -C CC=$(TARGET_CC) $(@D)
+NETPBM_DEPENDENCIES=libpng
+
 define NETPBM_BUILD_CMDS
-   make CC=$(TARGET_CC) STAGING_DIR=$(STAGING_DIR) -C $(@D)
+   make -C $(@D) CC=gcc buildtools/{endiangen,typegen,libopt}
+   make -C $(@D) CC=/opt/toolchain/toolchain-arm-linux/bin/arm-unknown-linux-uclibcgnueabi-gcc
 endef
 
 define NETPBM_INSTALL_TARGET_CMDS
+	cp $(@D)/converter/other/jpegtopnm $(TARGET_DIR)/usr/local/bin
+	cp $(@D)/analyzer/pamfile $(TARGET_DIR)/usr/local/bin
 endef
 
 define NETPBM_INSTALL_STAGING_CMDS
 endef
 
 define NETPBM_POST_PATCH_CMDS
-   cp $(TOPDIR)/package/netpbm/Makefile.config $(@D)
+   cp $(TOPDIR)/package/netpbm/config.mk $(@D)
 endef
 NETPBM_POST_PATCH_HOOKS+=NETPBM_POST_PATCH_CMDS
 
