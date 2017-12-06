ede83b8fe02d8531dd92a15ff3914f919f4faa8d
Maurice Ling <maurice@linglink.com>
2017-12-06 08:07:34 -0800

    IRV02SW-1 add copyImage.sh utility

---
+++ scripts/copyImage.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Script to assist in copying the image files to a target root directory.  Directory will be
+# named $targetDir/fsw_image-{version}
+#
+
+log()
+{
+    echo $*
+}
+
+#
+# Extract version from issue in usr_local tar file
+#
+versionDirName=$(tar -Oxvf output/images/fsw_image/usr_local.tar ./etc/issue | head -1|cut -d' ' -f 4-|sed -e "s/ - / /g;" -e "s/ /_/g;")
+fsw_image_root=output/images/fsw_image
+baseName=fsw_image
+satIp=${SAT_IP-irvine-02}
+target=root@$satIp
+targetRootDir=/data
+targetPath=${targetRootDir}/${baseName}-${versionDirName}
+ssh $target mkdir -p ${targetPath}
+if [ $? -ne 0 ]; then
+    log "[E] Unable to make $targetPath on $target"
+    exit 1
+fi
+
+log "[I] Copying image to ${target}:$targetPath"
+scp "${fsw_image_root}"/*.{bin,sh,tcl,jffs2} ${target}:${targetPath}
+if [ $? -ne 0 ]; then
+    log "[E] copy files to $target:${targetPath}"
+    exit 1
+fi
+
+echo "[I] Image copied to $targetPath on $target login and run install.sh, then reboot"
